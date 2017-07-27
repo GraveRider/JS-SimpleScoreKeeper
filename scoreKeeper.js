416fa92f8fc8a75f3599ac1ba91667551f4d09e3
GraveRider <donprayer@gmail.com>
2017-07-27 18:16:33 +0300

    Add files via upload

---
+++ scoreKeeper.js
@@ -0,0 +1,52 @@
+var p1 = document.querySelector("#p1");
+var p2 = document.querySelector("#p2");
+var resetButton = document.querySelector("#reset");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var victoryScoreDisplay = document.querySelector("p span");
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var victoryScore = 5;
+
+
+p1.addEventListener("click", function() {
+    if (!gameOver) {
+        p1Score++;
+        if (p1Score === victoryScore) {
+            p1Display.classList.add("winner");
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+});
+
+p2.addEventListener("click", function() {
+    if (!gameOver) {
+        p2Score++;
+        if (p2Score === victoryScore) {
+            p2Display.classList.add("winner");
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
+});
+
+resetButton.addEventListener("click", reset);
+
+numInput.addEventListener("change", function() {
+    victoryScoreDisplay.textContent = this.value;
+    victoryScore = Number(this.value);
+    reset();
+});
+
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
